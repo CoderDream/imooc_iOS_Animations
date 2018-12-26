6f30976744b6cacda023573f12e3d3bfd7f3a2f0
许林 <xulin.wh@qq.com>
2018-12-26 11:42:49 +0800

    position

---
+++ Animations/Animations/PositionViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PositionViewController.swift
+//  Animations
+//
+//  Created by CoderDream on 2018/12/25.
+//  Copyright © 2018 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+class PositionViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    
+    @IBOutlet weak var redSquare: UIView!
+    @IBOutlet weak var greenSquare: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1, animations: {
+            self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+            
+            self.redSquare.center.y = self.view.bounds.height - self.redSquare.center.y
+            
+            
+            self.greenSquare.center.x = self.view.bounds.width - self.greenSquare.center.x
+             self.greenSquare.center.y = self.view.bounds.height - self.greenSquare.center.y
+        })
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Animations/Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by CoderDream on 2018/12/25.
+//  Copyright © 2018 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
